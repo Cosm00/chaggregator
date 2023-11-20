6109600039200ff7da56e25ad4b60b743711fa12
Cosm00 <beach2399@gmail.com>
2023-11-20 12:15:01 -0800

    Update restream.py

--- classes/restream.py
+++ classes/restream.py
@@ -26,7 +26,7 @@ class Restream:
             eventPayload = messageData.get('payload', {}).get('eventPayload', {})
             formattedData = {
                 "msg": eventPayload.get('text', ''),
-                "author": eventPayload.get('author', {}).get('displayName', 'Unknown Author'),
+                "author": eventPayload.get('author', {}).get('displayName', eventPayload.get('author', {}).get('name', 'Unknown Author')),
                 "origin": self.parse_origin(messageData.get('payload', {}).get('connectionIdentifier', ''))
             }
             self.s.post('http://127.0.0.1:38293/chatMessage', json=formattedData)
